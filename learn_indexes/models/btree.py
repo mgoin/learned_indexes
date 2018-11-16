ed2787ea90771b7bc1cf37cbea02c2b41876ff6e
Aaron Young <ayoung3210@gmail.com>
2018-11-15 23:27:22 -0500

    Fixed btree model with np array predict.

--- learn_indexes/models/btree.py
+++ learn_indexes/models/btree.py
@@ -1,13 +1,21 @@
 """Wrapper for the BTree implementation so that it will fit within the testing framework."""
 from BTrees.IIBTree import IIBTree
 import models.utils as utils
+import numpy as np
 
 class BTree(IIBTree):
     def __init__(self):
         IIBTree.__init__(self)
 
     def predict(self, key):
-        super().get(self, key)
+        # Key is just a value, make it an array
+        if type(key) != np.ndarray:
+            key = np.full(1, key)
+
+        guess = np.zeros(len(key))
+        for i, k in enumerate(key):
+            guess[i] = super().get(self, k)
+        return guess
 
     def get(self, key, guess):
         return guess
