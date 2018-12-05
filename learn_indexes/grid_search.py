9c00b7da0d4421ef3bfe600ffe248749b320970d
Aaron Young <ayoung3210@gmail.com>
2018-12-05 14:44:07 -0500

    Added pool method for grid search.

--- learn_indexes/grid_search.py
+++ learn_indexes/grid_search.py
@@ -5,9 +5,10 @@ import sys
 from testing_framework import Testing_Framework
 from create_data import Distribution
 from models import Learned_Model, BTree, Hybrid, Hybrid_Original
+from multiprocessing import Pool
 
 
-def run_test(model, mp, tp, number_of_tests):
+def run_test(model, mp, tp, number_of_tests=1):
     m = model(**mp)
     tf = Testing_Framework(model=m, **tp)
     tf.run_tests(number_of_tests)
@@ -80,10 +81,14 @@ def main(argv):
 
                 grid_search.append((model, mp, tp))
 
-    # Perform grid search
+    # Perform grid search sequentially
     for i, (model, mp, tp) in enumerate(grid_search):
         print("Test {} of {}: {}, {}, {}".format(i, len(grid_search), model, sorted(mp.items()), sorted(tp.items())))
         run_test(model, mp, tp, number_of_tests)
+
+    # # Perform the grid search using a pool
+    # p = Pool(4)
+    # p.starmap(run_test, grid_search)
 
 
 if __name__ == '__main__':
